50c89f0877d0669bcab6be2e7bbafbfbab5cafbc
Rafael Karele <raphagbi@gmail.com>
2023-07-11 09:01:40 -0300

    Utilizando terraform e ansible

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f65b8614a860c29"
+  instance_type = "t2.micro"
+  key_name = "iac-curso"
+  # user_data = "${file("init.sh")}"
+  # user_data_replace_on_change = true
+  # vpc_security_group_ids = ["sg-07022039b72b63fd4"]
+  tags = {
+    Name = "Teste AWS"
+  }
+}
+
